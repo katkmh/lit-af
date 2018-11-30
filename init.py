ecc7fd6ce35dffa6fa185215d7da4acfc98ce526
katkmh <hernandez.kkm@gmail.com>
2018-11-30 18:04:55 +0800

    initialization of database

--- init.py
+++ init.py
@@ -1,47 +1,47 @@
 from test import db,Piece,Writer,List,Rating,Reviews
 db.create_all()
 
-piece = Piece(pieceID = 2000, writerID = 1, title= "A Bitch is Tired", genre= "Slice of Life", text = "And then she was tired")
-db.session.add(piece)
-piece = Piece(pieceID = 2001, writerID = 2, title= "A Koreaboo's Journey", genre= "Slice of Life", text = "Annyeonghaseyo")
-db.session.add(piece)
-piece = Piece(pieceID = 2002, writerID = 3, title= "Romeo and Juliet", genre= "Tragedy", text = "They both died.")
-db.session.add(piece)
-
-db.session.commit()
-
-user = Writer(writerID = 1, name = "Trina Aguilana", about = "Jaehyun", listID = 1000, password = "ayokona")
+user = Writer(name="Trina Aguilana", about="lalalala", password="ayokona")
 db.session.add(user)
-user = Writer(writerID = 2, name = "Kat Hernandez", about = "Doyoung", listID = 1001, password = "koreaboo")
+user = Writer(name="Kat Hernandez", about="sasadasdsasd", password="koreaboo")
 db.session.add(user)
-user = Writer(writerID = 3, name = "William Shakespeare", about = "Some dead guy.", listID = 1002, password = NULL)
+user = Writer(name="William Shakespeare", about="Some dead guy.", password="asasas")
 db.session.add(user)
 
 db.session.commit()
 
-user_list = List(listID = 1000, writerID = 1, pieceID = 2000)
+piece = Piece(writerID=1, title="Something", genre="Slice of Life", text="Konnichiwa")
+db.session.add(piece)
+piece = Piece(writerID=2, title="What", genre= "Slice of Life", text="Annyeonghaseyo")
+db.session.add(piece)
+piece = Piece(writerID =3, title= "Romeo and Juliet", genre= "Tragedy", text = "They both died.")
+db.session.add(piece)
+
+db.session.commit()
+
+user_list = List(writerID = 1, pieceID = 2)
 db.session.add(user_list)
-user_list = List(listID = 1001, writerID = 2, pieceID = 2001)
+user_list = List(writerID = 2, pieceID = 2)
 db.session.add(user_list)
-user_list = List(listID = 1002, writerID = 3, pieceID = 2000)
+user_list = List(writerID = 3, pieceID = 3)
 db.session.add(user_list)
 
 db.session.commit() 
 
-rating = Rating(writerID = 1, pieceID = 2000, rate = 1.4)
+rating = Rating(writerID = 1, pieceID = 2, rate = 1.4)
 db.session.add(rating)
-rating = Rating(writerID = 1, pieceID = 2001, rate = 5)
+rating = Rating(writerID = 2, pieceID = 2, rate = 5)
 db.session.add(rating)
-rating = Rating(writerID = 2, pieceID = 2000, rate = 3.4)
+rating = Rating(writerID = 3, pieceID = 3, rate = 3.4)
 db.session.add(rating)
 
 db.session.commit()
 
-review = Reviews(writerID = 1, pieceID = 2000, text = "it's ayt")
+review = Reviews(writerID = 1, pieceID = 1, text = "it's ayt")
 db.session.add(review)
-review = Reviews(writerID = 1, pieceID = 2001, text = "ewan")
+review = Reviews(writerID = 2, pieceID = 2, text = "ewan")
 db.session.add(review)
-review = Reviews(writerID = 2, pieceID = 2000, text = "lmao")
+review = Reviews(writerID = 3, pieceID = 3, text = "lmao")
 db.session.add(review)
 
 db.session.commit()
