aae5e41ffaec30742a4254170b4073ae4931b4ae
tbaguilana <tbaguilana@gmail.com>
2018-12-01 18:54:30 +0800

    can add pieces so yung sa html form action gawing /add_piece yas

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.mysql import LONGTEXT
-from flask import request, redirect, url_for, session, abort
+from flask import request, redirect, url_for, session, abort, flash
 import os
 from flask_cors import CORS, cross_origin
 from sqlalchemy.orm import sessionmaker
@@ -33,6 +33,8 @@ class Writer(db.Model):
 
 	def as_dict(self):
 		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+	# def get_id(self):
+	# 	return self.writerID
 
 class List(db.Model):
 	listID = db.Column(db.Integer,unique=True,nullable=False,primary_key=True,autoincrement=True)
@@ -56,15 +58,28 @@ class Reviews(db.Model):
 
 	def as_dict(self):
 		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-		
-@app.route('/')
 
-def home():
+
+@app.route('/')
+def start():
 	session['logged_in']= False
-	if not session.get('logged_in'):
-		return render_template('home.html')
-	else:
-		return "test"
+	return render_template('home.html')
+
+@app.route('/add_piece', methods=['POST'])
+def addPiece():
+	result = db.session.query(Writer).filter_by(name=session['username']).first()
+	title = request.form['title']
+	genre = request.form['genre']
+	text = request.form['text']
+
+	writer_id = result.writerID
+
+	user = {'username': result.name}
+	piece = Piece(writerID=writer_id,title=title,genre=genre,text=text)
+	db.session.add(piece)
+	db.session.commit()
+
+	return render_template('test.html', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -81,16 +96,16 @@ def login():
 		result = db.session.query(Writer).filter_by(name=POST_USERNAME, password=POST_PASSWORD).scalar()
 		if result:
 			session['logged_in'] = True
+			session['username'] = POST_USERNAME
 			return render_template('test.html', user=user)
 		else:
-			return home()
+			return start()
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	error = None
 	if request.form['username'] == ' ' or request.form['password'] == ' ':
-		error = 'Invalid'
-		return render_template('home.html')
+		flash('invalid')
+		return start()
 	else:
 		user = {'username': request.form['username']}
 		POST_USERNAME = request.form['username']
@@ -98,10 +113,14 @@ def signup():
 		result = db.session.query(Writer).filter_by(name=POST_USERNAME).scalar()
 
 		if result:
-			return render_template('home.html')
+			flash('error')
+			return start()
 		else:
 			writer = Writer(name=request.form['username'], about=request.form['about'], password=request.form['password'])
 			db.session.add(writer)
+			db.session.commit()
+			writer_list = List(writerID=writer.writerID)
+			db.session.add(writer_list)
 			db.session.commit()
 			session['logged_in'] = True
 			return render_template('test.html', user=user)
@@ -109,6 +128,7 @@ def signup():
 @app.route('/logout')
 def logout():
 	session['logged_in']= False
+	session.pop('username', None)
 	return home
 
 if __name__ == '__main__':
